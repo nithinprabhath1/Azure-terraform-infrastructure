0a052113971cb299fefe27f65eb60f79ef0890e1
Nithin Prabhath <Nithinprabhath1@gmail.com>
2026-06-12 09:57:07 +0530

    fix key vault access policy for Service Principal

--- 1. provider.tf
+++ 1. provider.tf
@@ -18,7 +18,7 @@ provider "azurerm" {
   features {
     resource_group {
       prevent_deletion_if_contains_resources = false
-    } 
+    }
   }
   subscription_id = "8f683f3b-6556-4979-a8b8-e0505f9a1303"
 }
